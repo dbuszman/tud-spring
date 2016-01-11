e2952d60443f40af7623098f60ca132ad1f00b69
Dariusz Buszman <dbuszman@hotmail.com>
2016-01-11 17:20:01 +0100

    Added model for Magazyn table

--- ProjectSpring/src/main/java/com/example/project_spring/domain/Magazyn.java
+++ ProjectSpring/src/main/java/com/example/project_spring/domain/Magazyn.java
@@ -1,9 +1,58 @@
 package com.example.project_spring.domain;
 
-public class Magazyn {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
-	public Magazyn() {
-		// TODO Auto-generated constructor stub
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "magazyn.all", query = "SELECT m FROM Magazyn m"),
+})
+
+public class Magazyn {
+	
+	private Long idPosition;
+	
+	private String name;
+	
+	private int amount;
+	
+	private int margin;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return idPosition;
 	}
 
+	public void setId(Long idPosition) {
+		this.idPosition = idPosition;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public int getMargin() {
+		return margin;
+	}
+
+	public void setMargin(int margin) {
+		this.margin = margin;
+	}
 }
